b1cf8d3ee663849a5389989885c6c93f40c57f70
Florence Afolabi <florence.afolabi@gmail.com>
2022-10-27 21:48:54 +0200

    adds function to remove numbers in play from board

--- math/app/Main.hs
+++ math/app/Main.hs
@@ -7,8 +7,14 @@ import Data.Function (on)
 import System.Random
 import Control.Monad(when, replicateM) 
 
-allBlankBoard :: [Maybe Int]
-allBlankBoard = [Nothing | _ <- [1..16]]
+-- stack run math-game
+
+-- allBlankBoard :: [Maybe Int]
+-- allBlankBoard = [Nothing | _ <- [1..16]]
+
+chooseNumbers :: [Maybe Int] -> [Int] -> [Maybe Int]
+chooseNumbers a b = a \\ map Just b
+
 
 printBoard :: [Maybe Int] -> IO()
 printBoard b =  (putStrLn.unwords.map showNumber) b -- alt: map $ show $ map b 
@@ -17,29 +23,29 @@ showNumber :: Maybe Int -> String
 showNumber (Just n) = show n
 showNumber _        = "-"
 
-addNumberToBoard :: [Maybe Int] -> Int -> [Maybe Int]
-addNumberToBoard (b:bs) _ = []
-addNumberToBoard b n =  init b ++ [Just n]
+-- addNumberToBoard :: [Maybe Int] -> Int -> [Maybe Int]
+-- addNumberToBoard (b:bs) _ = []
+-- addNumberToBoard b n =  init b ++ [Just n]
 
 -- method 1: prints random list of numbers, between 1 and 20
-randomListOne :: Int -> IO [Int]
-randomListOne n = sequence $ replicate n $ randomRIO (1,20::Int)
+-- randomListOne :: Int -> IO [Int]
+-- randomListOne n = sequence $ replicate n $ randomRIO (1,20::Int)
 
 -- method 2: prints random list of numbers, between 1 and 20
-randomListTwo :: Int -> IO([Int])
-randomListTwo 0 = return []
-randomListTwo n = do
-    r  <- randomRIO (1,20)
-    rs <- randomListTwo (n-1)
-    return (r:rs) 
+-- randomListTwo :: Int -> IO([Int])
+-- randomListTwo 0 = return []
+-- randomListTwo n = do
+--     r  <- randomRIO (1,20)
+--     rs <- randomListTwo (n-1)
+--     return (r:rs) 
 
 -- get s random number
-drawInt :: IO Int
-drawInt = getStdRandom(randomR (1, 20))
+-- drawInt :: IO Int
+-- drawInt = getStdRandom(randomR (1, 20))
 
 -- to do: use this after shuffling numbers
-createInitBoard :: [(Integer, Bool)]
-createInitBoard = zip [1..] $ take 20 (repeat False)
+-- createInitBoard :: [(Integer, Bool)]
+-- createInitBoard = zip [1..] $ take 20 (repeat False)
 
 main :: IO ()
 main = do
@@ -54,16 +60,18 @@ main = do
     -- printBoard updatedBoard
 
     -- prints random list of numbers
-    putStrLn "random numbers, between 1 and 20, with duplicates"
-    board2 <- randomListOne 20
-    let boardmap = map Just board2
-    printBoard boardmap
+    -- putStrLn "random numbers, between 1 and 20, with duplicates"
+    -- board2 <- randomListOne 20
+    -- let boardmap = map Just board2
+    -- printBoard boardmap
     -- prints random list ------
 
     -- prints a list of numbers between 1 and 20
     putStrLn "\nordered set of numbers, between 1 and 20"
     let b = map Just [1..20]
     printBoard b
+    let c = chooseNumbers b [1..4]
+    printBoard c
     -- to do: shuffle list
     -- to do: add bool to numbers, using createInitBoard
     -- to do: split into 16:4
